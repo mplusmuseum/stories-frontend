cd37b875e1959a07b72dbc188f033d7dcead9077
arnasziedavicius <arnas.ziedavicius@gmail.com>
2019-04-09 23:01:25 +0100

    Add ItemTalk

---
+++ src/components/ItemTalk.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="item item--talk">
+
+    <div class="item__inner shadow shadow--small">
+
+      <div class="item__media">
+
+        <talk-card
+        :talk="item"
+        :series="item.series"
+        showSection/>
+
+      </div>
+
+      <tag-group class="fs-s"
+      :tagIds="item.tags"/>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+import TalkCard from './TalkCard.vue';
+
+export default {
+  props: {
+    item: {
+      required: true,
+    },
+  },
+  components: {
+    TalkCard,
+  },
+};
+</script>
